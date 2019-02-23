7ba674e839aca1aab53170b843a2624ee28c2dc1
Curti.s_ <matthewscurtis.dev81@gmail.com>
2019-02-23 23:01:39 +0300

    [:sparkles: Update class-based views to use the generic class-based view] - Use of less code when using ListCreateAPIView and RetrieveUpdateDestroyAPIView

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,37 +1,19 @@
-from rest_framework import generics, mixins
+from rest_framework import generics
 from .models import Snippet
 from .serializers import SnippetSerializer
 
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin,
-                generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     """
     List all code snippets, or create new snippet
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, *kwargs):
-        return self.list(request,*args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class SnippetDetails(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetails(generics.RetrieveDestroyAPIView):
     """
     Retrieve, Update, Delete a code snippet
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-        
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-   
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
